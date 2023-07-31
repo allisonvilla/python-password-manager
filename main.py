c92d0b0f2fa93f60faaca537d250657c3dd2296e
Allison <allison.villa@outlook.com>
2023-07-31 16:11:33 -0400

    save entries in json format instead of txt, error catching and handling

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 import os
+import json
 
 
 def generate_password():
@@ -30,30 +31,52 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_entry = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
     
     if len(email) > 0:
         with open("recent_email.txt", "w") as email_file:
             email_file.write(email)
 
-    if len(website) == 0 or len(password) == 0:
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showinfo(title="Oops", message="Please fill in all the fields.")
     else:
         confirmed = messagebox.askokcancel(title=website,
                                            message=f"Do you want to save the following log-in? \nEmail: {email} \nPassword: {password}")
 
         if confirmed:
-            with open("passwords.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            current_data = {}
+            try:
+                with open("passwords.json", "r") as data_file:
+                    current_data = json.load(data_file)
+                    current_data.update(new_entry)
+            except FileNotFoundError:
+                current_data = new_entry
+            except json.decoder.JSONDecodeError:
+                current_data = new_entry
+            finally:
+                with open("passwords.json", "w") as data_file:
+                    json.dump(current_data, data_file, indent=4)
+                    website_entry.delete(0, END)
+                    password_entry.delete(0, END)
 
 
 def open_passwords_file():
-    os.startfile('passwords.txt')
+    try:
+        os.startfile('passwords.json')
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops", message="Save a log-in to generate a passwords file.")
 
 
-with open("recent_email.txt", "r") as email_text_file:
-    recent_email = email_text_file.readlines()
+try:
+    with open("recent_email.txt", "r") as email_text_file:
+        recent_email = email_text_file.readlines()
+except FileNotFoundError:
+    recent_email = ""
 
 window = Tk()
 window.title("Password Manager")
@@ -68,6 +91,8 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password")
 password_label.grid(row=3, column=0)
 
+# TODO: Make classes for inputs and buttons
+
 # Inputs
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
@@ -81,9 +106,9 @@ password_entry.grid(row=3, column=1, columnspan=2)
 
 # Buttons
 gen_password_btn = Button(text="Generate Password", width=29, command=generate_password)
-gen_password_btn.grid(row=4, column=1, columnspan=2)
-add_btn = Button(text="Save", width=29, command=save)
-add_btn.grid(row=5, column=1, columnspan=2)
+gen_password_btn.grid(row=4, column=1, columnspan=2, pady=(0, 20))
+save_btn = Button(text="Save Details", width=29, command=save)
+save_btn.grid(row=5, column=1, columnspan=2)
 open_btn = Button(text="Open Passwords File", width=29, command=open_passwords_file)
 open_btn.grid(row=6, column=1, columnspan=2)
 
