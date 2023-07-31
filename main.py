06837ef4ff7a9ef9c406224dd32f800da077de6e
Allison <allison.villa@outlook.com>
2023-07-31 18:15:44 -0400

    added buttons to copy email and password to clipboard

--- main.py
+++ main.py
@@ -73,6 +73,16 @@ def find_password():
                 messagebox.showinfo(title="Oops", message=f"No log-in details found for {website}.")
 
 
+def copy_email():
+    email = email_entry.get()
+    pyperclip.copy(email)
+
+
+def copy_password():
+    password = password_entry.get()
+    pyperclip.copy(password)
+
+
 def open_passwords_file():
     try:
         os.startfile('passwords.json')
@@ -98,7 +108,6 @@ def generate_password():
 
     new_password = "".join(password_list)
     password_entry.insert(0, new_password)
-    pyperclip.copy(new_password)
 
 
 try:
@@ -115,33 +124,47 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 window.eval('tk::PlaceWindow . center')
 
-# Labels
+# Website
 website_label = Label(text="Website")
 website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
+search_btn = Button(text="Search Current Entries", width=29, command=find_password)
+search_btn.grid(row=2, column=1, pady=(0, 15), columnspan=2)
+
+# Email
 email_label = Label(text="Email/Username")
 email_label.grid(row=3, column=0, pady=(0, 15))
-password_label = Label(text="Password")
-password_label.grid(row=4, column=0)
 
-# Inputs
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
-website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=27)
 email_entry.grid(row=3, column=1, pady=(0, 15))
 if recent_email:
     email_entry.insert(0, recent_email)
-password_entry = Entry(width=35)
+
+copy_email_btn = Button(text="Copy", width=5, command=copy_email)
+copy_email_btn.grid(row=3, column=2, pady=(0, 15))
+
+# Password
+password_label = Label(text="Password")
+password_label.grid(row=4, column=0)
+
+password_entry = Entry(width=27)
 password_entry.grid(row=4, column=1)
 
-# Buttons
-search_btn = Button(text="Search Current Entries", width=29, command=find_password)
-search_btn.grid(row=2, column=1, pady=(0, 15))
+copy_pw_btn = Button(text="Copy", width=5, command=copy_password)
+copy_pw_btn.grid(row=4, column=2)
+
 gen_password_btn = Button(text="Generate New Password", width=29, command=generate_password)
-gen_password_btn.grid(row=5, column=1, pady=(0, 30))
+gen_password_btn.grid(row=5, column=1, pady=(0, 30), columnspan=2)
+
+# Misc
 save_btn = Button(text="Save Details", width=29, command=save)
-save_btn.grid(row=6, column=1)
+save_btn.grid(row=6, column=1, columnspan=2)
+
 open_btn = Button(text="Open Passwords File", width=29, command=open_passwords_file)
-open_btn.grid(row=7, column=1)
+open_btn.grid(row=7, column=1, columnspan=2)
 
 window.mainloop()
